f826d8ae777c912575af0b67e2419517e61b1575
ChumaM <ChumaM@hollywoodbets.net>
2023-07-19 16:06:46 +0200

    Add project files.

---
+++ Controllers/MyConfigController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConfigurationSource.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyConfigController : Controller
+    {
+        public IConfiguration Configuration { get; }
+        public MyConfigController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+    
+
+        [HttpGet]
+        public Dictionary<string, string> Get()
+        {
+
+          return  Configuration.GetSection("Demo")
+                .GetChildren()
+                .ToDictionary(x=> x.Key, x => x.Value);
+
+
+
+        }
+    }
+}
